a7344ac3042e38963224680c63451077f611c1c8
Jamie Morris <jamie.morris@nielsen.com>
2019-08-12 17:01:29 +0100

    Use root dir of script for accessing scripts

--- index.sh
+++ index.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-source ./src/napDeployment.sh
-source ./src/dockerShortcuts.sh
-source ./src/gitShortcuts.sh
-source ./src/kubeShortcuts.sh
+ROOT_DIR=$( cd "$(dirname "$0")" ; pwd -P )
+
+source $ROOT_DIR/src/napDeployment.sh
+source $ROOT_DIR/src/dockerShortcuts.sh
+source $ROOT_DIR/src/gitShortcuts.sh
+source $ROOT_DIR/src/kubeShortcuts.sh
