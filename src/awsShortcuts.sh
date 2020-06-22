69c55a5fb5f5f4eb18c54d4c61692a873f06649b
Jamie Morris <jamie.morris@nielsen.com>
2020-06-22 09:47:05 +0100

    Added list buckets and objects helpers

--- src/awsShortcuts.sh
+++ src/awsShortcuts.sh
@@ -4,7 +4,7 @@ function createS3Bucket {
     USAGE="Usage: createS3Bucket <bucketName> [--localstack]"
     LOCALSTACK=false
 
-    for i in "$@" 
+    for i in "$@"
     do
         case $i in
             -ls|--localstack)
@@ -13,12 +13,11 @@ function createS3Bucket {
             -h|--help)
             echo "${logo}Create an S3 bucket optionally specifying whether to create one in localstack"
             echo "\t$USAGE\n"
-            printf "\t-ls,\t--localstack\t\t[default \"false\"]\n\t\t\t\tWhether to point at localstack url (http://localhost:4568)\n"
+            printf "\t-ls,\t--localstack\t\t[default \"false\"]\n\t\t\t\tWhether to point at localstack url (http://localhost:4566)\n"
             return
             ;;
         esac
     done
-
 
     if [ -z $1 ]; then
         echo "Invalid args\n\t$USAGE"
@@ -26,8 +25,70 @@ function createS3Bucket {
     fi
 
     if [ "$LOCALSTACK" = true ]; then
-        aws --endpoint-url=http://localhost:4568 s3api create-bucket --bucket $1
+        aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket $1
     else
-        aws s3api create-bucket --bucket $1    
+        aws s3api create-bucket --bucket $1
+    fi
+}
+
+function listS3Objects {
+    USAGE="Usage: listS3Objects <bucketName> [--localstack]"
+    LOCALSTACK=false
+
+    for i in "$@"
+    do
+        case $i in
+            -ls|--localstack)
+            LOCALSTACK=true
+            ;;
+            -h|--help)
+            echo "${logo}List items in an bucket optionally specifying whether to look at localstack"
+            echo "\t$USAGE\n"
+            printf "\t-ls,\t--localstack\t\t[default \"false\"]\n\t\t\t\tWhether to point at localstack url (http://localhost:4566)\n"
+            return
+            ;;
+        esac
+    done
+
+    if [ -z $1 ]; then
+        echo "Invalid args\n\t$USAGE"
+        return
+    fi
+
+    if [ "$LOCALSTACK" = true ]; then
+        aws --endpoint-url=http://localhost:4566 s3api list-objects-v2 --bucket $1
+    else
+        aws s3api list-objects-v2 --bucket $1
+    fi
+}
+
+function listS3Buckets {
+    USAGE="Usage: listS3Buckets [--localstack]"
+    LOCALSTACK=false
+
+    for i in "$@"
+    do
+        case $i in
+            -ls|--localstack)
+            LOCALSTACK=true
+            ;;
+            -h|--help)
+            echo "${logo}List items in an bucket optionally specifying whether to look at localstack"
+            echo "\t$USAGE\n"
+            printf "\t-ls,\t--localstack\t\t[default \"false\"]\n\t\t\t\tWhether to point at localstack url (http://localhost:4566)\n"
+            return
+            ;;
+        esac
+    done
+
+    if [ -z $1 ]; then
+        echo "Invalid args\n\t$USAGE"
+        return
+    fi
+
+    if [ "$LOCALSTACK" = true ]; then
+        aws --endpoint-url=http://localhost:4566 s3api list-buckets
+    else
+        aws s3api list-buckets
     fi
 }
