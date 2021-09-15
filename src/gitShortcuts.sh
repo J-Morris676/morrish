de2306768ff3cac9b75623ffd2ad93ee5b4cd216
Jamie Morris <jmorris@c02dx9x0md6m.corp.roku>
2021-09-15 09:19:43 +0100

    Add graph alias to git commands to display Git graph

--- src/gitShortcuts.sh
+++ src/gitShortcuts.sh
@@ -2,5 +2,6 @@
 
 function commit { git commit -m "$1"; }
 function commitall { git commit -am "$1"; }
+function graph { git log --graph --oneline --all }
 
 git config --global alias.tug pull # Because I'm 5
