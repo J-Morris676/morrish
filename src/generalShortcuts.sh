4f0e8a287562e3dd84911f0c16aa0cee62621756
Jamie Morris <jmorris@c02dx9x0md6m.corp.roku>
2021-09-21 11:09:32 +0100

    Add probeVideo, uses ffprobe to spit out video information

--- src/generalShortcuts.sh
+++ src/generalShortcuts.sh
@@ -4,3 +4,19 @@ function listDirectoryAndFileSizes {
 
     find $WHERE -maxdepth 1 -type d -mindepth 1 -exec du -hs {} \; | sort -rh
 }
+
+function probeVideo { 
+    if ! command -v ffprobe &> /dev/null
+    then
+        echo "ffprobe could not be found, install it."
+        return 1
+    fi
+
+    if [ -z "$1" ]
+    then
+        echo "Error: Please specify a video file: probeVideo <filepath>"
+        return 1
+    fi
+
+    ffprobe -v error -show_format -show_streams $1
+}
