5a4398602da263e39d01ee3e04917a2a756219b3
Jamie Morris <jmorris@c02dx9x0md6m.home>
2021-10-07 10:04:58 +0100

    Add squashAndRebase cli helper to squash all commits on a branch and rebase it off target branch

--- src/gitShortcuts.sh
+++ src/gitShortcuts.sh
@@ -18,4 +18,39 @@ function movetag {
     git push origin $TAG_NAME # Push tag to origin
 }
 
+function squashAndRebase { 
+    BASE_BRANCH="master"
+
+    for i in "$@"
+    do
+        case $i in
+            -bb=*|--base-branch=*)
+            export BASE_BRANCH=${i#*=}
+            ;;
+            -cm=*|--commit-message=*)
+            export COMMIT_MESSAGE=${i#*=}
+            ;;
+            -h|--help)
+            printf "${logo}Resets index to provided base-branch, squashing all commits into a single one provided with commit-message\n"
+            printf "\t${T_BLUE}-bb,\t--base-branch${T_RESET}\t\t[default \"master\"]\n\t\t\t\t\tThe base branch to reset index to\n"
+            printf "\t${T_BLUE}-cm,\t--commit-message${T_RESET}\tThe commit message of the newly squashed commit\n"
+            return
+            ;;
+        esac
+    done
+
+    if [ -z "$COMMIT_MESSAGE" ]
+    then
+        echo "Error: Please specify a commit message: squashAndRebase --commit-message=\"My changes\""
+        return 1
+    fi
+
+    CURRENT_BRANCH=$(git branch --show-current)
+    echo "Rebasing '$CURRENT_BRANCH' against '$BASE_BRANCH' and squashing commits with message '$COMMIT_MESSAGE'"
+
+    git reset $(git merge-base $BASE_BRANCH $CURRENT_BRANCH)
+    git add -A
+    git commit -m $COMMIT_MESSAGE
+}
+
 git config --global alias.tug pull # Because I'm 5
